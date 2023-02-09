0b3b9098d7f383b2eb34e3ceb22924d97a1e2b75
dsk-b <104132187+dsk-b@users.noreply.github.com>
2023-02-09 18:26:58 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
-import logo from './logo.svg';
+import React, { useState,useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [data, setdata] = useState(null);
+  useEffect(()=>{
+      fetch("http://localhost:2000/getconfigdata")
+      .then(res => res.json())
+      .then(val => setdata(val));
+  },[])
+  console.log(data);
   return (
     <div className="App">
       <header className="App-header">
